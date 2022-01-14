e720af8a6930dcca62281924afd2cd7953ed8bac
mefe-aydin <m.efeaydin@outlook.com>
2022-01-14 23:19:37 +0300

    add to cart part is done

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -10,12 +10,8 @@ import {
 import { AddShoppingCart } from "@material-ui/icons";
 import useStyles from "./styles"
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
-
-  // console.log(product);
-
-  // return <div>test</div>
 
   return (
     <Card className={classes.root}>
@@ -32,7 +28,7 @@ const Product = ({ product }) => {
             <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body2" color="textSecondary" />
       </CardContent>
       <CardActions disableSpacing className={classes.CardActions}>
-            <IconButton aria-label="Add to Cart">
+            <IconButton aria-label="Add to Cart" onClick={() =>onAddToCart(product.id,1)}>
                 <AddShoppingCart />
             </IconButton>
       </CardActions>
