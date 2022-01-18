f2cf28740c0a86d0dfe8e4dd1cd52e05fe8c21e2
mefe-aydin <m.efeaydin@outlook.com>
2022-01-18 16:39:28 +0300

    payment form & stripe was added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchProducts = async () =>{
     const { data } = await commerce.products.list();
@@ -41,6 +43,23 @@ const App = () => {
     setCart(cart);
   }
 
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart)
+  }
+
+  const handleCaputeCheckout = async (checkoutTokenId, newOrder) => {
+    try{
+        const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+        setOrder(incomingOrder);
+        refreshCart();
+    }catch(error){
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
   useEffect(() =>{
     fetchProducts();
     fetchCart();
@@ -59,7 +78,7 @@ const App = () => {
                 handleUpdateCartQty={handleUpdateCartQty} 
                 handleRemoveFromCart={handleRemoveFromCart} 
                 handleEmptyCart={handleEmptyCart} />} />
-          <Route exact path="/checkout" element={<Checkout cart={cart} />} />
+          <Route exact path="/checkout" element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaputeCheckout} error={errorMessage} />} />
         </Routes>
       </div>
     </Router>
